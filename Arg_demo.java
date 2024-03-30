83f6c625d747d33e490c8a5c685a74bdf4242bf6
AnshThakkar49 <148559109+AnshThakkar49@users.noreply.github.com>
2024-03-30 10:55:18 +0530

    Add files via upload

---
+++ Arg_demo.java
@@ -0,0 +1,13 @@
+public class Arg_demo 
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        b=Integer.parseInt(args[0]);
+        c=Integer.parseInt(args[1]);
+        a=b+c;
+        System.out.println(a);
+
+    }
+    
+}
